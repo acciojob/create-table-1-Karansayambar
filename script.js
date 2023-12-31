3ec7cb3fbfddf2be9917b674f2cfeb59893da100
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2023-12-31 11:08:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ function insert_Row() {
 	const tr = document.createElement('tr');
 	const td1 = document.createElement('td');
 	const td2 = document.createElement('td');
+	td1.textContent = '';
+    td2.textContent = '';
 	td1.textContent='New Cell1';
 	td2.textContent='New Cell2';
 	tr.appendChild(td1);
