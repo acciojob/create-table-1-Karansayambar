cd11958222517f77977b37235dc422d20d9ef7a5
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2023-12-31 11:30:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 function insert_Row() {
-    const addRow = document.getElementById('sampleTable');
-    const tr = document.createElement('tr');
-    const td1 = document.createElement('td');
-    const td2 = document.createElement('td');
+    // const addRow = document.getElementById('sampleTable');
+    // const tr = document.createElement('tr');
+    // const td1 = document.createElement('td');
+    // const td2 = document.createElement('td');
 
-    td1.textContent = '';
-    td2.textContent = '';
+    // td1.textContent = '';
+    // td2.textContent = '';
 
-    td1.textContent = 'New Cell1';
-    td2.textContent = 'New Cell2';
+    // td1.textContent = 'New Cell1';
+    // td2.textContent = 'New Cell2';
 
-    tr.appendChild(td1);
-    tr.appendChild(td2);
+    // tr.appendChild(td1);
+    // tr.appendChild(td2);
 
-    addRow.insertBefore(tr, addRow.firstChild);
+    // addRow.insertBefore(tr, addRow.firstChild);
+	var x=document.getElementById('sampleTable').insertRow(0);
+	var y = x.insertCell(0); 
+	var z = x.insertCell(1);
+	y.innerHTML="New Cell1";
+	z.innerHTML="New Cell2";
 }
 
