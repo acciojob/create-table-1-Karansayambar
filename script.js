ed9c1373e7434fc4fe63d3a0d56afbe692476992
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2023-12-31 11:19:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,19 +4,15 @@ function insert_Row() {
     const td1 = document.createElement('td');
     const td2 = document.createElement('td');
 
-    // Clear existing content of the cells
     td1.textContent = '';
     td2.textContent = '';
 
-    // Set new content for the cells
     td1.textContent = 'New Cell1';
     td2.textContent = 'New Cell2';
 
-    // Append the table cells to the table row
     tr.appendChild(td1);
     tr.appendChild(td2);
 
-    // Insert the new row at the top of the table
     addRow.insertBefore(tr, addRow.firstChild);
 }
 
